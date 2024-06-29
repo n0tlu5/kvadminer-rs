6ce6fe17240ef5898074d6b990509dd10cf1de8a
n0tlu5 <darfisultoni@gmail.com>
2024-06-29 22:49:41 +0700

    add expired session cleanup bg task; add session timeout to appstate; remove unused import

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use actix_files::NamedFile;
 use actix_web::{web, App, HttpServer};
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use tokio::time::{interval, Duration};
 use log::info;
 use env_logger::Env;
 
@@ -12,21 +12,30 @@ mod handlers;
 mod session;
 
 use handlers::*;
-use session::AppState;
+use session::{AppState, cleanup_expired_sessions};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     info!("Starting the server...");
 
-    let app_state = web::Data::new(AppState {
+    let app_state = Arc::new(AppState {
         connections: Arc::new(Mutex::new(HashMap::new())),
-        session_timeout: std::time::Duration::from_secs(3600), // 1 hour timeout
+        session_timeout: Duration::from_secs(3600), // 1 hour timeout
+    });
+
+    let app_state_clone = app_state.clone();
+    tokio::spawn(async move {
+        let mut interval = interval(Duration::from_secs(60)); // Cleanup interval
+        loop {
+            interval.tick().await;
+            cleanup_expired_sessions(app_state_clone.clone()).await;
+        }
     });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(app_state.clone())
+            .app_data(web::Data::from(app_state.clone()))
             .route("/", web::get().to(|| serve_html("./static/index.html")))
             .route(
                 "/keys-management",
